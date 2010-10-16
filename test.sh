e2f2b0ceeb1fe5a978139877c72bc7be3b6e299a
Ivan Mincik <devnull@localhost>
2010-10-16 21:01:53 +0200

    * Updating tests.

--- test.sh
+++ test.sh
@@ -18,9 +18,9 @@ find . -type f -iname "*.sql" -exec psql ghistory -f "{}" \;
 
 psql ghistory -c "SELECT gis.G_CreateGISHistory('gis', 'v_layer');"
 
-psql ghistory -c "INSERT INTO gis.v_layer(aaa) VALUES (111);"
-psql ghistory -c "INSERT INTO gis.v_layer(bbb) VALUES ('bbb');"
-psql ghistory -c "INSERT INTO gis.v_layer(ccc) VALUES (true);"
+psql ghistory -c "INSERT INTO gis.v_layer(aaa, the_geom) VALUES (111, (SELECT ST_GeomFromText('MULTILINESTRING((10 10,20 20))')));"
+psql ghistory -c "INSERT INTO gis.v_layer(bbb, the_geom) VALUES ('bbb', (SELECT ST_GeomFromText('MULTILINESTRING((15 15,25 25))')));"
+psql ghistory -c "INSERT INTO gis.v_layer(ccc, the_geom) VALUES (true, (SELECT ST_GeomFromText('MULTILINESTRING((20 20,30 30))')));"
 psql ghistory -c "SELECT * FROM gis.v_layer;"
 psql ghistory -c "SELECT * FROM gis_history.hist_gis_v_layer;"
 echo "**************************************************************************************"
@@ -35,4 +35,4 @@ psql ghistory -c "UPDATE gis.v_layer SET aaa = '100000' WHERE aaa = 111;"
 psql ghistory -c "SELECT * FROM gis.v_layer;"
 psql ghistory -c "SELECT * FROM gis_history.hist_gis_v_layer;"
 
-psql ghistory -c   "select * FROM v_layer_AtTime('2999-1-1');"
+psql ghistory -c   "SELECT * FROM v_layer_AtTime('2999-1-1');"
