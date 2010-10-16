aa29f2783613287895821cc3801643183d99de04
Ivan Mincik <devnull@localhost>
2010-10-17 00:20:50 +0200

    * Retrieving table metadata using 'information_schema'.

--- function_G_GetTableFields.sql
+++ function_G_GetTableFields.sql
@@ -7,23 +7,9 @@ dbtable = args[1]
 vars = {'dbschema': dbschema, 'dbtable': dbtable} 
 
 sql = """
-SELECT a.attnum AS ordinal_position,
-	a.attname AS column_name,
-	t.typname AS data_type,
-	a.attlen AS char_max_len,
-	a.atttypmod AS modifier,
-	a.attnotnull AS notnull,
-	a.atthasdef AS hasdefault,
-	adef.adsrc AS default_value
-FROM pg_class c
-JOIN pg_attribute a ON a.attrelid = c.oid
-JOIN pg_type t ON a.atttypid = t.oid
-JOIN pg_namespace nsp ON c.relnamespace = nsp.oid
-LEFT JOIN pg_attrdef adef ON adef.adrelid = a.attrelid AND adef.adnum = a.attnum
-WHERE
-	nspname = '%(dbschema)s' AND c.relname = '%(dbtable)s' AND
-	a.attnum > 0
-ORDER BY a.attnum;
+SELECT column_name FROM information_schema.columns
+	WHERE table_schema = '%(dbschema)s' AND table_name = '%(dbtable)s'
+	ORDER BY ordinal_position;
 """ % vars
 ret = plpy.execute(sql)
 
